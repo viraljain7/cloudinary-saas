8ea9aba531ee6292135689fae484289b71e2e583
viraljain7 <veerljain1234@gmail.com>
2024-08-16 23:26:33 +0530

    feat: integrate Clerk for user authentication

---
+++ middleware.ts
@@ -0,0 +1,34 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isPublicRoute = createRouteMatcher(["/signin", "/signup", "/", "/home"]);
+const isPublicApiRoute = createRouteMatcher(["/api/videos"]);
+
+export default clerkMiddleware((auth, req) => {
+  const { userId } = auth();
+  const currentUrl = new URL(req.url);
+  const isHomePage = currentUrl.pathname === "/home";
+  const isApiRequest = currentUrl.pathname.startsWith("/api");
+  // If user is logged in and accessing a public route but not the dashboard
+  if (userId && isPublicRoute(req) && !isHomePage) {
+    return NextResponse.redirect(new URL("/home", req.url));
+  }
+
+  //not logged in
+  if (!userId) {
+    // If user is not logged in and trying to access a protected route
+    if (!isPublicRoute(req) && !isPublicApiRoute(req)) {
+      return NextResponse.redirect(new URL("/signin", req.url));
+    }
+
+    // If the request is for a protected API and the user is not logged in
+    if (isApiRequest && !isPublicApiRoute(req)) {
+      return NextResponse.redirect(new URL("/signin", req.url));
+    }
+  }
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
